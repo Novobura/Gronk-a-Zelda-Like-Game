16e4e16a348eab1add0ca164b0a26abbf5c4b38b
Novobura <novobura@gmail.com>
2022-07-01 13:44:52 +0900

    Update level.py

--- Zelda-Like/Project/Code/level.py
+++ Zelda-Like/Project/Code/level.py
@@ -23,7 +23,9 @@ class Level:
                 y = row_index * TILESIZE
                 if col == 'x':
                     Tile((x,y),[self.visible_sprites])
-
+                if col == 'p':
+                    Player((x,y),[self.visible_sprites])
+                    
     def run(self):
         #update and draw the Game
         self.visible_sprites.draw(self.display_surface)
